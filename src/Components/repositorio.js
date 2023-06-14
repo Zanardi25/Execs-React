905749da347e04313640c0dd129a3a0f07e99f9e
Zanardi25 <103010186+Zanardi25@users.noreply.github.com>
2023-06-14 10:59:38 -0300

    Add files via upload

---
+++ src/Components/repositorio.js
@@ -0,0 +1,7 @@
+export default function repositorio() {
+    return (
+        <>
+             <a href="https://github.com/Zanardi25/Execs-React" target="_blank">Link do repositorio do github</a>
+        </>
+    ) 
+}
