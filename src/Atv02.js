d9f9d8810b20ebabaf5a1b7aceed72cc8953bc6e
Zanardi25 <103010186+Zanardi25@users.noreply.github.com>
2023-06-14 10:16:13 -0300

    Update Atv02.js

--- src/Atv02.js
+++ src/Atv02.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Atv01(){
     return (
         <>
-          <h2> Atividade 01</h2>
+          <h2> Atividade 02</h2>
           <Link className="voltar" to="/"> Voltar</Link>
           <Calculadora/>
         </>
