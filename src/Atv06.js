e275ba780656c5c1066dc792e95df8d325edca71
Zanardi25 <103010186+Zanardi25@users.noreply.github.com>
2023-06-14 10:33:30 -0300

    Update Atv06.js

--- src/Atv06.js
+++ src/Atv06.js
@@ -1,11 +1,13 @@
 import Calculadora from "./Components/calculadora";
 import { Link } from "react-router-dom";
+import './calculdora.css';
 
 export default function Atv06(){
     return (
         <>
           <h2> Atividade 06</h2>
           <Link className="voltar" to="/"> Voltar</Link>
+          
           <Calculadora/>
         </>
     );
