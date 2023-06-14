5b993a0d810bef737fddc0d156e1f04b7b87e2b6
Zanardi25 <103010186+Zanardi25@users.noreply.github.com>
2023-06-14 10:21:39 -0300

    Update and rename Atv02.js to Atv06.js

--- src/Atv06.js
+++ src/Atv06.js
@@ -1,10 +1,10 @@
 import Calculadora from "./Components/calculadora";
 import { Link } from "react-router-dom";
 
-export default function Atv01(){
+export default function Atv06(){
     return (
         <>
-          <h2> Atividade 02</h2>
+          <h2> Atividade 06</h2>
           <Link className="voltar" to="/"> Voltar</Link>
           <Calculadora/>
         </>
