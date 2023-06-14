fe72b8b0bbb7304283e94128d75345510118814c
Zanardi25 <103010186+Zanardi25@users.noreply.github.com>
2023-06-14 09:13:30 -0300

    Delete react-app directory

--- react-app/src/Components/Profile.js
+++
@@ -1,9 +0,0 @@
-export default function Profile(){
-    return (
-        <img 
-            src="https://i.imgur.com/Up8cUQN.jpeg"
-            alt="Katherine Johnson" 
-            style={{ width: '150px', height: '200px' }}
-        />
-    )
-}
